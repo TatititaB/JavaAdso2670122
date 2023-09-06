9bd1090c63dea6fbbfb33665b56b3c64a84a4075
Blaek11700 <kiguerrero5@soy.sena.edu.co>
2023-09-05 19:02:55 -0500

    creacion de proyecto java

---
+++ Adso2670122/src/adso2670122/Adso2670122.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package adso2670122;
+
+/**
+ *
+ * @author SENA
+ */
+public class Adso2670122 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
